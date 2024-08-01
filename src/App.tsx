199e5d8352fa72a5ba4ce2f58691b66cd91ee566
Juliana <julidiossa20@gmail.com>
2024-08-01 11:41:34 +0200

    Creación de titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>Juliana</div>;
+  return (
+    <div className="home">
+      <div className="home__title">
+        <h1>Dogs App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
